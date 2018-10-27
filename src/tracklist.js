0aa60b3b9e7aa81d5147d9139318762e8f91211a
Michael Cohen <michaelcohen716@gmail.com>
2018-10-27 19:28:02 -0400

    start building playlist

---
+++ src/tracklist.js
@@ -0,0 +1,61 @@
+export default tracklist = {
+    // Hip Hop
+    BigPoppaRemix: {
+        name: 'Big Poppa (Vandelux Remix)',
+        artist: 'Audio Aquarium',
+        playlist: 'Hip Hop',
+        source: 'BigPoppa.mp3',
+        next: 'HolUp'
+    },
+
+    HolUp: {
+        name: 'Hol \'Up',
+        artist: 'Kendrick Lamar',
+        playlist: 'Hip Hop',
+        source: 'HolUp.mp3',
+        next: 'Grammy'
+    },
+
+    Grammy: {
+        name: 'Grammy (Soulja Boy and Ester Dean Cover)',
+        artist: 'Purity Ring',
+        playlist: 'Hip Hop',
+        source: 'Grammy.mp3',
+        next: 'CantTellMeNothing'
+    },
+
+    CantTellMeNothing: {
+        name: 'Can\'t Tell Me Nothing',
+        artist: 'Kanye West',
+        playlist: 'Hip Hop',
+        source: 'CantTellMeNothing.mp3',
+        next: 'Hermitude'
+    },
+
+    // Electronica
+    Hermitude: {
+        name: 'Hermitude - HyperParadise (Flume Remix)',
+        artist: 'Flume',
+        playlist: 'Electronica',
+        source: 'Hermitude.mp3',
+        next: 'RunningWild'
+    },
+
+    RunningWild: {
+        name: 'Running Wild (Jayceeoh Remix)',
+        artist: 'Morgan Page',
+        playlist: 'Electronica',
+        source: 'RunningWild.mp3',
+        next: 'OneDayTheyllKnow'
+    },
+
+    OneDayTheyllKnow: {
+        name: 'One Day They\'ll Know(ODESZA Remix)',
+        artist: 'Pretty Lights',
+        playlist: 'Electronica',
+        source: 'OneDayTheyllKnow.mp3',
+        next: ''
+    },
+
+    
+}
